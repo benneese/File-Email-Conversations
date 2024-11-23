a34316781fb00b6756af4dfce42220a2be41476a
benneese <ben.neese@meriton.com>
2024-11-23 17:46:29 -0600

    deleted commented out section

--- script.js
+++ script.js
@@ -2,22 +2,6 @@ Office.initialize = function () {
 	console.log("Office initialized");
 };
 
-// function runJavaScript(event) {
-// 	// Your JavaScript logic here
-// 	console.log("Button clicked!");
-
-// 	// Example: Display a simple message
-// 	Office.context.mailbox.item.notificationMessages.addAsync("action", {
-// 		type: "informationalMessage",
-// 		message: "JavaScript executed successfully!",
-// 		icon: "icon16",
-// 		persistent: false,
-// 	});
-
-// 	// Call event.completed when done
-// 	event.completed();
-// }
-
 async function runJavaScript(event) {
 	try {
 		await moveEmailToConversationFolder();
